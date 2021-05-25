133799efec24cf8627323623d2977a49d85141b9
slang <slang@bethel.jw.org>
2021-05-25 17:21:24 +0200

    using TTP223

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -1,128 +1,163 @@
 #include <Arduino.h>
-#include <ADCTouch.h>
 #include "LowPower.h"
 #include <SoftPWM.h>
-int ref0; //reference values to remove offset
 
-const uint8_t adc1 = A3;
-// const uint8_t adc2 = A4;
-const uint8_t trigger = 10;
-const uint32_t calibinterval = 10000;
-const uint32_t standbyDelay = 20000;
+const uint32_t standbyDelay = 2000;
 uint32_t lastLight;
 bool direction;
+const uint8_t touchPin = 2;
+const uint8_t carPin = 3;
+const uint8_t FETPort = 5;
+volatile bool touched = false;
+volatile bool carTriggered = false;
+
+void PWMSet(uint8_t duty, bool hardset = false)
+{
+    SoftPWMSet(LED_BUILTIN, duty, hardset);
+    SoftPWMSet(FETPort, duty, hardset);
+}
+
+uint8_t PWMGet(bool raw = false)
+{
+    if (raw)
+    {
+        return SoftPWMGetRaw(LED_BUILTIN);
+    }
+    return SoftPWMGet(LED_BUILTIN);
+}
+bool PWMisFading()
+{
+    return SoftPWMisFading(LED_BUILTIN);
+}
 
 void flash(int8_t pin)
 {
-    auto current = SoftPWMGet(pin);
-    auto currentRaw = SoftPWMGetRaw(pin);
-    SoftPWMSet(pin, currentRaw < 128 ? currentRaw + 64 : 0, true);
+    auto current = PWMGet();
+    auto currentRaw = PWMGet(true);
+    PWMSet(currentRaw < 128 ? currentRaw + 64 : 0, true);
     delay(10);
-    SoftPWMSet(pin, currentRaw, true);
-    SoftPWMSet(pin, current, false);
-}
-void calibrate()
-{
-    flash(LED_BUILTIN);
-    ref0 = ADCTouch.read(adc1, 1000);
+    PWMSet(currentRaw, true);
+    PWMSet(current, false);
 }
 
-bool getTouch()
+bool isTouchTriggered()
 {
-    return trigger < (ADCTouch.read(adc1, 500) - ref0);
-}
-
-bool isTouchTriggered(bool touch)
-{
-    static bool active;
     static uint32_t lastTouch;
 
-    if (millis() - lastTouch > calibinterval)
+    if (!touched)
     {
-        lastTouch = millis();
-        calibrate();
-    }
-
-    if (!touch)
-    {
-        active = false;
         return false;
     }
+    touched = false;
 
-    if (active || (millis() - lastTouch < 50))
+    if (millis() - lastTouch < 50)
         return false;
     lastTouch = millis();
-    active = true;
     return true;
+}
+
+bool isCarTriggered()
+{
+    static uint32_t lastCarTrigger;
+
+    if (!carTriggered)
+    {
+        return false;
+    }
+    carTriggered = false;
+
+    if (millis() - lastCarTrigger < 500)
+        return false;
+    lastCarTrigger = millis();
+
+    return true;
+}
+
+void ISR0()
+{
+    touched = true;
+}
+void ISR1()
+{
+    carTriggered = true;
 }
 
 void setup()
 {
+    pinMode(touchPin, INPUT);
+    pinMode(carPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(touchPin), ISR0, RISING);
+    attachInterrupt(digitalPinToInterrupt(carPin), ISR1, FALLING);
+
     SoftPWMBegin();
-    SoftPWMSet(LED_BUILTIN, 0);
-    // SoftPWMSetFadeTime(LED_SIGNAL, 200, 200);
-    SoftPWMSetFadeTime(LED_BUILTIN, 4000, 4000);
-    // SoftPWMSet(LED_SIGNAL, 255);
-    // SoftPWMSet(LED_SIGNAL, 0);
+    PWMSet(0);
+    SoftPWMSetFadeTime(ALL, 4000, 4000);
 
     Serial.begin(115200);
-
-    calibrate();
+}
+void evalStandby()
+{
+    if (PWMGet(true) > 0)
+    {
+        lastLight = millis();
+    }
+    else if (millis() - lastLight > standbyDelay)
+    {
+        Serial.println("\nstdby");
+        delay(50);
+        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+        Serial.println("woke");
+        lastLight = millis();
+    }
 }
 
 void loop()
 {
-    if (isTouchTriggered(getTouch()))
+    if (isCarTriggered())
+    {
+        Serial.println("door");
+        PWMSet(255);
+    }
+    else if (isTouchTriggered())
     {
         flash(LED_BUILTIN);
-        uint8_t value = SoftPWMGetRaw(LED_BUILTIN);
+        uint8_t value = PWMGet(true);
         switch (value)
         {
         case 0:
             Serial.print("0");
             direction = HIGH;
-            SoftPWMSet(LED_BUILTIN, 255);
+            PWMSet(255);
             break;
         case 255:
             Serial.print("5");
             direction = LOW;
-            SoftPWMSet(LED_BUILTIN, 0);
+            PWMSet(0);
             break;
 
         default:
-            if (SoftPWMisFading(LED_BUILTIN))
+            if (PWMisFading())
             {
                 Serial.print("s");
-                SoftPWMSet(LED_BUILTIN, SoftPWMGetRaw(LED_BUILTIN));
+                PWMSet(PWMGet(true));
             }
             else
             {
                 if (direction == HIGH)
                 {
                     Serial.print("d");
-                    SoftPWMSet(LED_BUILTIN, 0);
+                    PWMSet(0);
                 }
                 else
                 {
                     Serial.print("u");
-                    SoftPWMSet(LED_BUILTIN, 255);
+                    PWMSet(255);
                 }
                 direction = !direction;
             }
-
             break;
         }
     }
-    if (SoftPWMGet(LED_BUILTIN) > 0)
-    {
-        lastLight = millis();
-    }
 
-    if (millis() - lastLight > standbyDelay)
-    {
-        Serial.println("stdby");
-        LowPower.powerStandby(SLEEP_4S, ADC_OFF, BOD_OFF);
-    }
-
-    // delay(10);
+    evalStandby();
 }
