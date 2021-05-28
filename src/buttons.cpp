44c98ad8444c8ea0192f04436dff3f3c9eddcb86
slang <slang@bethel.jw.org>
2021-05-28 12:48:24 +0200

    car trigger fixed

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "LowPower.h"
-// #include <SoftPWM.h>
 #include <FadeLed.h>
 
 const uint32_t standbyDelay = 2000;
@@ -12,31 +11,12 @@ const uint8_t carALight = A1;
 const uint8_t carABat = A2;
 const uint8_t FETPort = 5;
 volatile bool _touched = false;
-volatile int8_t _carTrigger = 0;
+volatile bool _carTrigger = false;
 #define OPENED 1
 #define CLOSED -1
 #define FADETIME 5000
 
 FadeLed led(FETPort);
-
-// void PWMSet(uint8_t duty, bool hardset = false)
-// {
-//     SoftPWMSet(LED_BUILTIN, duty, hardset);
-//     SoftPWMSet(FETPort, duty, hardset);
-// }
-
-// uint8_t PWMGet(bool raw = false)
-// {
-//     if (raw)
-//     {
-//         return SoftPWMGetRaw(LED_BUILTIN);
-//     }
-//     return SoftPWMGet(LED_BUILTIN);
-// }
-// bool PWMisFading()
-// {
-//     return SoftPWMisFading(LED_BUILTIN);
-// }
 
 void flash()
 {
@@ -59,11 +39,6 @@ bool isTouchTriggered()
 {
     static uint32_t lastTouch;
 
-    // if (!_touched)
-    // {
-    //     return false;
-    // }
-
     if (millis() - lastTouch < 200)
         _touched = false;
     else if (_touched)
@@ -73,20 +48,29 @@ bool isTouchTriggered()
 
 int8_t isCarTriggered()
 {
+#define ROUNDS 10
     static uint32_t lastCarTrigger;
-
-    // if (_carTrigger == 0)
-    // {
-    //     return 0;
-    // }
-    // auto t = _carTrigger;
+    int8_t ret = 0;
 
     if (millis() - lastCarTrigger < 100)
-        _carTrigger = 0;
+        _carTrigger = false;
     else if (_carTrigger)
+    {
+        delay(50); // let cap discharge
+        uint16_t res = 0;
+        for (size_t i = 0; i < ROUNDS; i++)
+        {
+            delay(10);
+            auto r = analogRead(carALight);
+            Serial.println(r);
+            res += r;
+        }
+        ret = res < 250 * ROUNDS ? OPENED : CLOSED;
+        _carTrigger = false;
         lastCarTrigger = millis();
+    }
 
-    return _carTrigger;
+    return ret;
 }
 void evalStandby()
 {
@@ -107,16 +91,10 @@ void ISR0()
 {
     _touched = true;
 }
-volatile uint16_t res;
+
 void ISR1()
 {
-#define ROUNDS 10
-    res = 0;
-    for (size_t i = 0; i < ROUNDS; i++)
-    {
-        res += analogRead(carALight);
-    }
-    _carTrigger = res < 250 * ROUNDS ? OPENED : CLOSED;
+    _carTrigger = true;
 }
 
 void setup()
@@ -126,14 +104,7 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(touchPin), ISR0, RISING);
     attachInterrupt(digitalPinToInterrupt(carPin), ISR1, CHANGE);
 
-    // SoftPWMBegin();
-    // PWMSet(0);
-    // SoftPWMSetFadeTime(ALL, 4000, 4000);
-
-    //Set update interval to 10ms
     FadeLed::setInterval(10);
-
-    //set fade time to 4 seconds
     led.setTime(FADETIME);
 
     Serial.begin(115200);
@@ -147,14 +118,12 @@ void loop()
     if (carTrigger == OPENED)
     {
 
-        Serial.print(res);
-        Serial.println(" door-opened");
+        Serial.println("opened");
         led.on();
     }
     else if (carTrigger == CLOSED)
     {
-        Serial.print(res);
-        Serial.println(" door-closed");
+        Serial.println("closed");
         led.off();
     }
     else if (isTouchTriggered())
@@ -172,44 +141,6 @@ void loop()
             Serial.print(direction ? "u" : "d");
             led.set(direction ? 100 : 0);
         }
-
-        // flash(LED_BUILTIN);
-        // auto value = led.getCurrent();
-        // switch (value)
-        // {
-        // case 0:
-        //     Serial.print("9");
-        //     direction = HIGH;
-        //     led.on();
-        //     break;
-        // case 100:
-        //     Serial.print("0");
-        //     direction = LOW;
-        //     led.off();
-        //     break;
-
-        // default:
-        //     if (!led.done())
-        //     {
-        //         Serial.print("s");
-        //         PWMSet(PWMGet(true));
-        //     }
-        //     else
-        //     {
-        //         if (direction == HIGH)
-        //         {
-        //             Serial.print("d");
-        //             led.off();
-        //         }
-        //         else
-        //         {
-        //             Serial.print("u");
-        //             led.on();
-        //         }
-        //         direction = !direction;
-        //     }
-        //     break;
-        // }
     }
 
     evalStandby();
