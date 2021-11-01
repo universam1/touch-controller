fcf7cac874fdb28825493ee045adbf1f17ff7a25
Samuel Lang <gh@lang-sam.de>
2021-11-01 07:18:50 +0100

    maxpowforvalue

---
+++ tools/pow.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"math"
+)
+
+//----------------------------------------------------------------------------------------------------
+// void findMaxForPow(uint16_t maxInputValue) {
+var exp float64 = 1
+var expLast float64 = 0
+var expadj float64 = 1
+var v float64 = 0
+
+// 	while (exp != expLast) {
+// 	  expLast = exp;
+// 	  v = pow(maxInputValue, exp);
+// 	  if (v > MAX_PWM_VALUE) {
+// 		exp -= expadj;
+// 		expadj = expadj / 10;
+// 	  }
+// 	  exp += expadj;
+// 	}
+// 	Serial.print(F("For a maximum input value of: "));
+// 	Serial.print(maxInputValue);
+// 	Serial.print(F("  Use: "));
+// 	Serial.print(exp, 10);
+// 	Serial.println(F("  In pow(..) function "));
+//   }
+
+const MAX_PWM_VALUE = 0xFFFF
+
+func main() {
+	for maxInputValue := 10; maxInputValue < 0xffff; maxInputValue++ {
+		go func(maxInputValue int) {
+			var exp float64 = 1
+			var expLast float64 = 0
+			var expadj float64 = 1
+			var v float64
+			i := 0
+			for exp != expLast {
+				i++
+				expLast = exp
+				v = math.Pow(float64(maxInputValue), exp)
+				if v > MAX_PWM_VALUE {
+					exp -= expadj
+					expadj = expadj / 10
+				}
+				exp += expadj
+			}
+			// fmt.Printf("For %d use %.15f - took %d\n", maxInputValue, exp, i)
+		}(maxInputValue)
+	}
+}
